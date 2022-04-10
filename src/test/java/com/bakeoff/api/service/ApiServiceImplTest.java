ac59b48800f649e47d2d481194a6df31e6c8cdc0
Callum Nightingale <callum.nightingale@capgemini.com>
2022-04-10 19:06:12 +0100

    test: mock the clock in tests

--- src/test/java/com/bakeoff/api/service/ApiServiceImplTest.java
+++ src/test/java/com/bakeoff/api/service/ApiServiceImplTest.java
@@ -17,11 +17,12 @@ import com.bakeoff.api.repositories.JudgeRepository;
 import com.bakeoff.api.repositories.ParticipantRepository;
 import com.bakeoff.api.repositories.ResultRepository;
 import java.time.Clock;
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.List;
 import mockit.Expectations;
 import mockit.Injectable;
-import mockit.Tested;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -52,8 +53,9 @@ class ApiServiceImplTest {
         .fkBakeoff(bakeoff)
         .build();
   }
-  
-  private Result createResult(Integer id, Participant participant, Judge judge, Integer taste, Integer appearance) {
+
+  private Result createResult(Integer id, Participant participant, Judge judge, Integer taste,
+      Integer appearance) {
     return Result.builder()
         .id(id)
         .fkParticipant(participant)
@@ -74,17 +76,18 @@ class ApiServiceImplTest {
         @Injectable BakeoffRepistory bakeoffRepistory,
         @Injectable JudgeRepository judgeRepository,
         @Injectable BakerRepository bakerRepository,
-        @Injectable ParticipantRepository participantRepository,
-        @Injectable Clock clock,
-        @Tested ApiServiceImpl apiService
+        @Injectable ParticipantRepository participantRepository
     ) {
-
+      Clock clock = Clock.fixed(Instant.parse("2021-01-01T10:10:10.00Z"), ZoneId.systemDefault());
+      ApiService apiService = new ApiServiceImpl(resultRepository, bakerRepository, judgeRepository,
+          bakeoffRepistory, participantRepository, clock);
+      
       Judge judge1 = createJudge(1, "Zach");
       Judge judge2 = createJudge(2, "Bella");
 
       Baker baker1 = createBaker(1, "Callum");
       Baker baker2 = createBaker(2, "Harry");
-      
+
       Bakeoff bakeoff = Bakeoff.builder()
           .boDate(LocalDate.of(2021, 1, 1))
           .food("Cheesecake")
@@ -93,7 +96,7 @@ class ApiServiceImplTest {
       Participant participant1 = createParticipant(1, 1, "Vanilla", baker1, bakeoff);
       Participant participant2 = createParticipant(2, 2, "Lemon", baker2, bakeoff);
       bakeoff.setParticipants(List.of(participant1, participant2));
-      
+
       Result result1 = createResult(1, participant1, judge1, 10, 5);
       Result result2 = createResult(2, participant1, judge2, 9, 4);
       Result result3 = createResult(3, participant2, judge1, 8, 3);
@@ -107,17 +110,17 @@ class ApiServiceImplTest {
         bakeoffRepistory.findAll();
         result = bakeoffs;
       }};
-      
+
       BakeoffResponseDto actual = apiService.getLatestBakeoff();
       assertEquals(1, actual.getBakeoffs().size());
-      
+
       BakeoffDto response = actual.getBakeoffs().get(0);
       assertAll(
           () -> assertEquals(LocalDate.of(2021, 1, 1), response.getDate()),
           () -> assertEquals("Cheesecake", response.getTitle()),
           () -> assertEquals(2, response.getParticipants().size())
       );
-      
+
       ParticipantDto p1 = response.getParticipants().get(0);
       ParticipantDto p2 = response.getParticipants().get(1);
       assertAll(
