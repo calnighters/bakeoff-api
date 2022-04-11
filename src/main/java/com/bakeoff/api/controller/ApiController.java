0aba2094e2157b9a443116a4b99ce2472d167f8c
Callum Nightingale <callum.nightingale@capgemini.com>
2022-04-11 13:56:03 +0100

    fix: add cross origin from web app

--- src/main/java/com/bakeoff/api/controller/ApiController.java
+++ src/main/java/com/bakeoff/api/controller/ApiController.java
@@ -3,6 +3,7 @@ package com.bakeoff.api.controller;
 import com.bakeoff.api.dto.BakeoffResponseDto;
 import com.bakeoff.api.service.ApiService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "https://bakeoff-web.herokuapp.com")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/bakeoff")
